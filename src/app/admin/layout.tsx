751ee7714b975683a17960d53e6728fcae6eb049
Albert <albertnsemo27@gmail.com>
2026-06-29 11:59:41 +0100

    fix waitlist page 2

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -13,7 +13,10 @@ export default async function AdminLayout({
   return (
     <div className="min-h-screen bg-[#FDFAF7] lg:flex">
       <AdminNav />
-      <div className="min-w-0 flex-1 pb-8">{children}</div>
+      {/* FIXED: Constrained width tightly on mobile to prevent layout blowouts */}
+      <div className="w-full max-w-full min-w-0 flex-1 pb-8 overflow-x-hidden">
+        {children}
+      </div>
     </div>
   );
 }
